5570dde50c553dc1cd45df6da4e4761599228b3d
Curt Lynch <curjlync@ut.utm.edu>
2023-03-02 12:38:30 -0600

    Add project files.

---
+++ PiDarts/FindPiThread.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PiDarts
+{
+    internal class FindPiThread
+    {
+        int darts;
+        int hits;
+        Random rand;
+
+        public int Hits { get { return hits; } }
+
+        public FindPiThread(int darts)
+        {
+            this.darts = darts;
+            hits = 0;
+            rand = new Random();
+        }
+
+        public void throwDarts()
+        {
+            double x, y, hypotenuse;
+            while (darts > 0)
+            {
+                x = rand.NextDouble();
+                y = rand.NextDouble();
+                hypotenuse = Math.Sqrt(Math.Pow(x, 2.0) + Math.Pow(y, 2));
+                if (hypotenuse <= 1)
+                {
+                    hits++;
+                }
+                darts--;
+            }
+        }
+    }
+}
---
+++ PiDarts/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PiDarts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num_darts = 1, num_threads = 1, hits = 0;
+            Stopwatch watch = new Stopwatch();
+
+            Console.WriteLine("PiDarts Pi Calculator");
+            Console.WriteLine("Enter the number of dart throws per thread.");
+            num_darts = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number of threads to use");
+            num_threads = int.Parse(Console.ReadLine());
+            Console.WriteLine("Calculating please wait...");
+            watch.Start();
+
+            List<Thread> threads = new List<Thread>();
+            List<FindPiThread> dart_boards = new List<FindPiThread>();
+
+            for (int i = 0; i < num_threads; i++)
+            {
+                FindPiThread board = new FindPiThread(num_darts);
+                dart_boards.Add(board);
+                Thread thread = new Thread(new ThreadStart(board.throwDarts));
+                threads.Add(thread);
+                thread.Start();
+                Thread.Sleep(16);
+            }
+
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
+
+            foreach(FindPiThread board in dart_boards)
+            {
+                hits += board.Hits;
+            }
+
+            double pi = 4.0 * ((double)hits / (num_darts * num_threads));
+            watch.Stop();
+            Console.WriteLine("Pi calculation complete!");
+            Console.WriteLine("Pi calculated to be " + pi);
+            Console.WriteLine("This calculation took " + watch.Elapsed.TotalSeconds + " seconds to complete.");
+            Console.ReadKey();
+        }
+    }
+}
